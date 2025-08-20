c25fb312fbcec002bf82cdb157243aa5b016da6a
bittoshimoto <bitcoremototoshi@proton.me>
2025-08-20 17:19:35 +0200

    paths

--- electrum/util.py
+++ electrum/util.py
@@ -697,11 +697,11 @@ def user_dir():
     elif 'ANDROID_DATA' in os.environ:
         return android_data_dir()
     elif os.name == 'posix':
-        return os.path.join(os.environ["HOME"], ".electrum")
+        return os.path.join(os.environ["HOME"], ".rabbit")
     elif "APPDATA" in os.environ:
-        return os.path.join(os.environ["APPDATA"], "Electrum")
+        return os.path.join(os.environ["APPDATA"], "Rabbit")
     elif "LOCALAPPDATA" in os.environ:
-        return os.path.join(os.environ["LOCALAPPDATA"], "Electrum")
+        return os.path.join(os.environ["LOCALAPPDATA"], "Rabbit")
     else:
         #raise Exception("No home directory found in environment variables.")
         return
